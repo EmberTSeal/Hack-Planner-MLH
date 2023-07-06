7c4be20f02a8e3f1ab0128e35b9921a485b91aa0
EmberTSeal <trishaseal49@gmail.com>
2023-07-06 23:50:40 +0530

    email remainder feature added

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
 # import functionality into our project so we don't have to write it   
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, jsonify
 import sqlite3
+import os
+import sendgrid
+from sendgrid.helpers.mail import Mail
+
+sendgrid_api_key = os.getenv('SENDGRID_API_KEY')
+reminder_email = os.getenv('REMINDER_EMAIL')
+
 
 app = Flask(__name__)
 
@@ -83,3 +90,31 @@ def edit(item_id):
 def delete(item_id):
     delete_item(item_id)
     return redirect('/')
+
+def send_email(subject, body):
+     message = Mail(
+        from_email = reminder_email,
+        to_emails = 'trisha.seal.22@aot.edu.in',
+        subject = subject,
+        plain_text_content = body
+     )
+     try: 
+         sg = sendgrid.SendGridAPIClient(api_key= sendgrid_api_key)
+         response = sg.send(message)
+         if response.status_code == 202:
+            return True
+     except Exception as e:
+         print("An error occured while sending email :", str(e))
+     return False
+
+@app.route('/send_email', methods = ['POST'])
+def send_email_route():
+    data = request.get_json()
+    item = data['item']
+    subject = "Reminder: {}".format(item)
+    body = "This is a reminder for the task: {}".format(item)
+    if send_email(subject, body):
+        return jsonify(success = True)
+    else:
+        return jsonify(success = False), 500
+    
